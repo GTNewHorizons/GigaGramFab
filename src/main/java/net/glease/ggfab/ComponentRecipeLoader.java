f9d1ef6958bae1203e2b2ebd69f2f8d28a3e9fcc
Glease <4586901+Glease@users.noreply.github.com>
2023-01-03 20:33:04 +0800

    add linked input bus

--- src/main/java/net/glease/ggfab/ComponentRecipeLoader.java
+++ src/main/java/net/glease/ggfab/ComponentRecipeLoader.java
@@ -1,8 +1,12 @@
 package net.glease.ggfab;
 
+import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -33,5 +37,14 @@ class ComponentRecipeLoader implements Runnable {
                 1200,
                 6000
         );
+        RA.addAssemblerRecipe(new ItemStack[]{
+                        ItemList.Hatch_Input_Bus_IV.get(1L),
+                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Elite, 4L),
+                        GT_Utility.getIntegratedCircuit(12),
+                },
+                Materials.Enderium.getMolten(1296L),
+                GGItemList.LinkedInputBus.get(1L),
+                600,
+                (int) GT_Values.VP[5]);
     }
 }
